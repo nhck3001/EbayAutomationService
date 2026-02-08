2568e67c057832aa745e7d060a42a2f0b042bb4e
nhck3001 <88605167+nhck3001@users.noreply.github.com>
2026-02-08 00:08:42 -0500

    Now can actually use production API
    v^1.1#i^1#r^1#f^0#p^3#I^3#t^Ul41XzY6OEZDMkJGMzQ4MjQ5ODlBMzY1RkYwOTRENzk0NkJBRkJfMF8xI0VeMjY

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using EbayAutomationService.Services;
 using DotNetEnv;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
+using System.Linq;
 
 class Program
 {
@@ -13,12 +15,26 @@ class Program
         var appId = Environment.GetEnvironmentVariable("APP_ID");
         var certId = Environment.GetEnvironmentVariable("CLIENT_SECRET");
         var refreshToken = Environment.GetEnvironmentVariable("EBAY_REFRESH_TOKEN");
-        
+
         // Get the access token by using the EbayAuthService object
         Console.WriteLine("\n----------Requesting access token...----------");
         var ebayAuthService = new EbayAuthService(appId!, certId!, refreshToken!);
-        var x = 10;
-        
+        var ebayTokenManager = new EbayTokenManager(ebayAuthService);
+        var httpClient = new HttpClient();
+        var ebayAPIClient = new EbayApiClient(httpClient, ebayTokenManager);
+        var ebayBrowseService = new EbayBrowseApiService(ebayAPIClient);
+        var list1 = await ebayBrowseService.GetItemIdsBySeller("parwazcollections");
+        var list2 = await ebayBrowseService.GetItemIdsBySeller("parwazcollections", limit: 50, offset: 50);
+        var list3 = await ebayBrowseService.GetItemIdsBySeller("parwazcollections", offset: 100);
+        var list4 = await ebayBrowseService.GetItemIdsBySeller("parwazcollections", offset: 150);
+        var list5 = await ebayBrowseService.GetItemIdsBySeller("parwazcollections", offset: 200);
+        // Get all itemId in a list
+        var totalList = list1.Concat(list2).Concat(list3).Concat(list4).Concat(list5).Distinct().ToList();
+        foreach (var itemId in totalList)
+        {
+            var item = await ebayBrowseService.GetItemByItemId(itemId);
+        }
+
         //// Making call to the EbayUserService API to retrieve user info
         //var userService = new EbayUserService(ebayAPIClient);
         //var userInfo = await userService.GetUserInfo();
---
+++ Services/BrowseApi/EbayBrowseApiService.cs
@@ -0,0 +1,104 @@
+using System.Text;
+using EbayAutomationService.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// This class is used to talk to the Inventory API
+public class EbayBrowseApiService
+{
+    private readonly EbayApiClient _api;
+
+    public EbayBrowseApiService(EbayApiClient api)
+    {
+        _api = api;
+    }
+    /// <summary>
+    /// Fetch public listings from a specific eBay seller (no keyword filter)
+    /// </summary>
+    /// <param name="sellerUsername">The eBay seller username</param>
+    /// <param name="limit">Max items per request (<= 50)</param>
+    /// <param name="offset">Pagination offset</param>
+    /// <returns>List of listing summaries</returns>
+    public async Task<List<string>> GetItemIdsBySeller(
+        string sellerUsername,
+        int limit = 50,
+        int offset = 0)
+    {
+        var itemIds = new List<string>();
+
+        var filterValue = $"sellers:{{{sellerUsername}}}";
+        var encodedFilter = Uri.EscapeDataString(filterValue);
+
+        var url =
+            $"https://api.ebay.com/buy/browse/v1/item_summary/search" +
+            $"?q=%20" +
+            $"&limit={limit}" +
+            $"&offset={offset}" +
+            $"&filter={encodedFilter}";
+
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        var response = await _api.SendAsync(request);
+        var responseJson = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException(
+                $"eBay Browse API failed with status code {response.StatusCode}. Response: {responseJson}",
+                null,
+                response.StatusCode
+            );
+        }
+
+        var obj = JObject.Parse(responseJson);
+
+        var items = obj["itemSummaries"];
+        if (items == null)
+            return itemIds;
+
+        foreach (var item in items)
+        {
+            var itemId = item["itemId"]?.ToString();
+            if (!string.IsNullOrEmpty(itemId))
+            {
+                itemIds.Add(itemId);
+            }
+        }
+
+        return itemIds;
+    }
+
+    // Get info of an item based on itemId
+    public async Task<JObject> GetItemByItemId(string itemId, string marketplaceId = "EBAY_US")
+    {
+        if (string.IsNullOrWhiteSpace(itemId))
+            throw new ArgumentException("itemId is required");
+
+        var url = $"https://api.ebay.com/buy/browse/v1/item/{itemId}";
+
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        // Required for Browse API
+        request.Headers.Add("X-EBAY-C-MARKETPLACE-ID", marketplaceId);
+
+        var response = await _api.SendAsync(request);
+        var responseJson = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException(
+                $"Browse Item API failed with status code {response.StatusCode}. Response: {responseJson}",
+                null,
+                response.StatusCode
+            );
+        }
+
+        var x = JObject.Parse(responseJson);
+        var y = 10;
+        return x;
+    }
+
+
+
+
+}
