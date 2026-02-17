0b740245c0e671d8699369ff4f8f5d8b4c04795c
nhck3001 <88605167+nhck3001@users.noreply.github.com>
2026-02-17 05:31:42 -0500

    Create AppDbContext.cs

---
+++ Infrastructure/AppDbContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+// This class acts as a bridge between C# code and database
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    // Each DbSet<T> is a database table that can be queries
+    // The => Set<T>() is a shorthand that returns the DbSet for that entity type.
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<ProductPid> ProductPids => Set<ProductPid>();
+    public DbSet<Sku> Skus => Set<Sku>();
+    public DbSet<Listing> Listings => Set<Listing>();
+
+    // This function applies all the configuration that has been built 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductPidConfiguration());
+        modelBuilder.ApplyConfiguration(new SkuConfiguration());
+        modelBuilder.ApplyConfiguration(new ListingConfiguration());
+    }
+}
