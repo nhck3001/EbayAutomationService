bd9478313633f45955db54d9174c94937b0aa921
nhck3001 <88605167+nhck3001@users.noreply.github.com>
2026-03-04 00:19:45 -0500

    Add a new is_populated column to categories table

---
+++ Migrations/20260304051438_CategoryTableNewColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EbayAutomationService.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryTableNewColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_populated",
+                table: "categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_populated",
+                table: "categories");
+        }
+    }
+}
